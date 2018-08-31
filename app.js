628eaf52ca3a69a55645c37f74c0e8825c86a6cf
vbhardwa <vbhardwa@CND5463FPV>
2018-08-31 11:53:32 +0530

    resolved auth issue

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
+
+require('dotenv').config();
   
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
